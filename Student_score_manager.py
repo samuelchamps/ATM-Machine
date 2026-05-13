e3626a33c0e1539278fcca71a962703c2af11f8e
samuel champs <omojiadesamuel@gmail.com>
2026-05-13 12:06:49 +0100

    remove multiple files from GitHub tracking

--- Student_score_manager.py
+++
@@ -1,7 +0,0 @@
-"""
-Manage a list of student scores. 
-Add new scores, remove the lowest, sort the list, and calculate the class average.
-
-"""
-score = [72, 55, 88, 91, 63, 47, 79]
-score.append(float(input("Enter a new student's score: ")))
